7f1555894f4b1ca4545f8880bbff256192a6dda3
folarin oyenuga <folami25@gmail.com>
2022-08-01 22:10:29 +0100

    a recap of basic python concepts

---
+++ basic1.py
@@ -0,0 +1,11 @@
+# name = "tom"
+# name += " riddle"
+# print(name)
+
+
+# miles ran
+print("How many miles did you cover?")
+kms = float(input())
+miles = kms * 0.621371
+miles = round(miles, 2)
+print(f"Your {kms}km walk is equal to {miles}ml.")
